eb4feaadfd6ba509b5d56ed808190906be881894
Yassine <mahfoudh.yassin2000@gmail.com>
2025-09-11 10:17:22 +0100

    fix #2

--- components/hero.tsx
+++ components/hero.tsx
@@ -68,7 +68,7 @@ export function Hero() {
                 playsInline
                 poster="/images/mission_1.jpg" // Image de fallback
               >
-                <source src="/images/Promotion-video.mp4" type="video/mp4" />
+                <source src="/videos/Promotion-video.mp4" type="video/mp4" />
                 Votre navigateur ne supporte pas la lecture vidéo.
               </video>
 
